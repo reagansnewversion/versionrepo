d08b05e3460f71989880a1a4d0b4e0dc51552995
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-12-14 20:40:43 -0600

    continued work on dex controller

--- app/controllers/DexController.php
+++ app/controllers/DexController.php
@@ -4,12 +4,14 @@ class DexController extends BaseController {
 
 	public function index()
 	{
-
+		$pokemon = Pokemon::all();
+		return View::make('dexindex');
 	}
 
-	public function show()
+	public function show($number)
 	{
-		
+		$pokemon = DB::table('pokemon')->where('number', $number)->get();
+		return View::make('dexshow')->with('pokemon', $pokemon);
 	}
 }
 ?>
