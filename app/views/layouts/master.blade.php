f9a4c0b794f7888304f3d07a2e8b7fc6d5f1a306
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-12-14 19:20:04 -0600

    added in bootstrap and jquery and started work on the master layout

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>New Version</title>
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
+</head>
+<body>
+@yield('content')
+<script type="text/javascript" src="/js/jquery.js"></script>
+</body>
+</html>
