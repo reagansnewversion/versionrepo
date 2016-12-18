e36fdf02a6b09fe7c3f269c9a4bc4f13ea9c084e
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-12-18 16:38:28 -0600

    Began adding the required functionality for the menu

--- app/views/dexindex.blade.php
+++ app/views/dexindex.blade.php
@@ -11,7 +11,32 @@
 		@endif
 	</div>
 @endforeach
-<div class="menu">This is the menu</div>
+<div class="menu">
+	<div class="menurow col-lg-12 col-md-12">
+		<div class="col-lg-5 col-lg-offset-1 col-md-5">
+			Pokemon
+		</div>
+		<div class="col-lg-5 col-lg-offset-1 col-md-5">
+			Pokedex
+		</div>
+	</div>
+	<div class="menurow col-lg-12 col-md-12">
+		<div class="col-lg-5 col-lg-offset-1 col-md-5">
+			Bag
+		</div>
+		<div class="col-lg-5 col-lg-offset-1 col-md-5">
+			Trainer Card
+		</div>
+	</div>
+	<div class="menurow col-lg-12 col-md-12">
+		<div class="col-lg-5 col-lg-offset-1 col-md-5">
+			Options
+		</div>
+		<div class="col-lg-5 col-lg-offset-1 col-md-5">
+			Save
+		</div>
+	</div>
+</div>
 @stop
 @section('bottom-script')
 <script type="text/javascript" src="/js/menu.js"></script>
