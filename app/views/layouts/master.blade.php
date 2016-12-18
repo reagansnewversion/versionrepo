f468d01128b8499ce03ef78bd3150dcce3bab99a
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-12-18 15:47:04 -0600

    Began testing possible menu opening/closing solutions

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -12,10 +12,17 @@
 <script type="text/javascript">
 $(document).ready(function() {
 
+	var status = "Closed";
 	$(document).keyup(function(event) {
 		var input = event.which;
 		if(input == 74) {
-			console.log("Menu Opened");
+			if(status == "Closed") {
+				console.log("Menu Opened");
+				status = "Open";
+			} else {
+				console.log("Menu Closed");
+				status = "Closed";
+			}
 		}
 	});
 });
