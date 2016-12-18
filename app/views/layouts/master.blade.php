8c92203df953ba83ce05fac6d569d24d25904cea
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-12-18 15:50:21 -0600

    Added sections to the master layout to allow for top-script and bottom-script

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -4,10 +4,12 @@
 	<title>New Version</title>
 	<link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="/css/version.css">
+	@yield('top-script')
 </head>
 <body>
 @yield('content')
 <script type="text/javascript" src="/js/jquery.js">
 </script>
+@yield('bottom-script')
 </body>
 </html>
