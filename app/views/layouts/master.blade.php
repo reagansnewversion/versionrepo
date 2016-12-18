3a4513d488a28752b1b85ba4fa99f016b54fa5f2
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-12-18 15:44:39 -0600

    Began testing possible menu solutions

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -7,6 +7,18 @@
 </head>
 <body>
 @yield('content')
-<script type="text/javascript" src="/js/jquery.js"></script>
+<script type="text/javascript" src="/js/jquery.js">
+</script>
+<script type="text/javascript">
+$(document).ready(function() {
+
+	$(document).keyup(function(event) {
+		var input = event.which;
+		if(input == 74) {
+			console.log("Menu Opened");
+		}
+	});
+});
+</script>
 </body>
 </html>
