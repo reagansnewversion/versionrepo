5f78e78a6824dbda6e6c32b6d05bdd9c5c06151d
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-12-18 15:49:02 -0600

    Moved successful menu test code to its own js file

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -9,23 +9,5 @@
 @yield('content')
 <script type="text/javascript" src="/js/jquery.js">
 </script>
-<script type="text/javascript">
-$(document).ready(function() {
-
-	var status = "Closed";
-	$(document).keyup(function(event) {
-		var input = event.which;
-		if(input == 74) {
-			if(status == "Closed") {
-				console.log("Menu Opened");
-				status = "Open";
-			} else {
-				console.log("Menu Closed");
-				status = "Closed";
-			}
-		}
-	});
-});
-</script>
 </body>
 </html>
---
+++ public/js/menu.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+	var status = "Closed";
+	$(document).keyup(function(event) {
+		var input = event.which;
+		if(input == 74) {
+			if(status == "Closed") {
+				console.log("Menu Opened");
+				status = "Open";
+			} else {
+				console.log("Menu Closed");
+				status = "Closed";
+			}
+		}
+	});
+});
