9850f7f54c0cd84daf6cbc764d16fca430d5de97
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-12-18 16:28:18 -0600

    Successfully added pausing functionality while menu is open

--- public/js/menu.js
+++ public/js/menu.js
@@ -5,9 +5,11 @@ $(document).ready(function() {
 		if(input == 74) {
 			if(status == "Closed") {
 				$(".menu").css('opacity', 1);
+				$("body").css("overflow", "hidden");
 				status = "Open";
 			} else {
 				$(".menu").css('opacity', 0);
+				$("body").css("overflow", "auto");
 				status = "Closed";
 			}
 		}
