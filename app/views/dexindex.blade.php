fb7f95784be9e13b8e8ab53df5c617034d7c1018
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-12-18 19:28:17 -0600

    Need to set up a function to make code DRYer and also handle z-indexes

--- app/views/dexindex.blade.php
+++ app/views/dexindex.blade.php
@@ -13,36 +13,50 @@
 @endforeach
 <div class="menu">
 	<div class="menurow text-center col-lg-12 col-md-12">
-		<div id="pokemon" class="menubutton col-lg-6 col-md-6">
+		<button type="submit" id="pokemon" class="menubutton pokemon col-lg-6 col-md-6">
 			Pokemon
-		</div>
-		<div id="pokedex" class="menubutton col-lg-6 col-md-6">
+		</button>
+		<button id="pokedex" class="menubutton col-lg-6 col-md-6">
 			Pokedex
-		</div>
+		</button>
 	</div>
 	<div class="menurow text-center col-lg-12 col-md-12">
-		<div id="bag" class="menubutton col-lg-6 col-md-6">
+		<button id="bag" class="menubutton col-lg-6 col-md-6">
 			Bag
-		</div>
-		<div id="card" class="menubutton col-lg-6 col-md-6">
+		</button>
+		<button id="card" class="menubutton col-lg-6 col-md-6">
 			Trainer Card
-		</div>
+		</button>
 	</div>
 	<div class="menurow text-center col-lg-12 col-md-12">
-		<div id="options" class="menubutton col-lg-6 col-md-6">
+		<button id="options" class="menubutton col-lg-6 col-md-6">
 			Options
-		</div>
-		<div id="save" class="menubutton col-lg-6 col-md-6">
+		</button>
+		<button id="save" class="menubutton col-lg-6 col-md-6">
 			Save
-		</div>
+		</button>
 	</div>
 </div>
-<div class="pokemonmenu">Pokemon</div>
-<div class="pokedexmenu">Pokedex</div>
-<div class="bagmenu">Bag</div>
-<div class="cardmenu">Trainer Card</div>
-<div class="optionsmenu">Options</div>
-<div class="savemenu">Save</div>
+<div class="pokemonmenu">
+	Pokemon
+	<button class="back">Back</div>
+</div>
+<div class="pokedexmenu">
+	Pokedex
+	<button class="back">Back</button>
+</div>
+<div class="bagmenu">Bag
+	<button class="back">Back</button>
+</div>
+<div class="cardmenu">Trainer Card
+	<button class="back">Back</button>
+</div>
+<div class="optionsmenu">Options
+	<button class="back">Back</button>
+</div>
+<div class="savemenu">Save
+	<button class="back">Back</button>
+</div>
 @stop
 @section('bottom-script')
 <script type="text/javascript" src="/js/menu.js"></script>
--- public/js/menu.js
+++ public/js/menu.js
@@ -4,26 +4,82 @@ $(document).ready(function() {
 		var input = event.which;
 		if(input == 74) {
 			if(status == "Closed") {
+				console.log("Menu opened");
 				$(".menu").css('opacity', 1);
 				$("body").css("overflow", "hidden");
-				$("#pokemon").click(function() {
 
+			$(".menu").click(function() {
+				console.log("It worked");
+			});
+			$(".pokemon").click(function() {
+				console.log("Pokemon");
+				$(".menu").css('opacity', 0);
+				$(".menu").css('z-index', 0);
+				$(".pokemonmenu").css("opacity", 1);
+				$(".pokemonmenu").css('z-index', 1);
+				$(".pokedexmenu").css("opacity", 0);
+				$(".bagmenu").css("opacity", 0);
+				$(".cardmenu").css("opacity", 0);
+				$(".optionsmenu").css("opacity", 0);
+				$(".savemenu").css("opacity", 0);
+					});
+			$("#pokedex").click(function() {
+				$(".menu").css('opacity', 0);
+				$(".pokemonmenu").css("opacity", 0);
+				$(".pokedexmenu").css("opacity", 1);
+				$(".bagmenu").css("opacity", 0);
+				$(".cardmenu").css("opacity", 0);
+				$(".optionsmenu").css("opacity", 0);
+				$(".savemenu").css("opacity", 0);
+			});
+			$("#bag").click(function() {
+				$(".menu").css('opacity', 0);
+				$(".pokemonmenu").css("opacity", 0);
+				$(".pokedexmenu").css("opacity", 0);
+				$(".bagmenu").css("opacity", 1);
+				$(".cardmenu").css("opacity", 0);
+				$(".optionsmenu").css("opacity", 0);
+				$(".savemenu").css("opacity", 0);
+			});
+			$("#card").click(function() {
+				$(".menu").css('opacity', 0);
+				$(".pokemonmenu").css("opacity", 0);
+				$(".pokedexmenu").css("opacity", 0);
+				$(".bagmenu").css("opacity", 0);
+				$(".cardmenu").css("opacity", 1);
+				$(".optionsmenu").css("opacity", 0);
+				$(".savemenu").css("opacity", 0);
+			});
+			$("#options").click(function() {
+				$(".menu").css('opacity', 0);
+				$(".pokemonmenu").css("opacity", 0);
+				$(".pokedexmenu").css("opacity", 0);
+				$(".bagmenu").css("opacity", 0);
+				$(".cardmenu").css("opacity", 0);
+				$(".optionsmenu").css("opacity", 1);
+				$(".savemenu").css("opacity", 0);
+			});
+			$("#save").click(function() {
+				$(".menu").css('opacity', 0);
+				$(".pokemonmenu").css("opacity", 0);
+				$(".pokedexmenu").css("opacity", 0);
+				$(".bagmenu").css("opacity", 0);
+				$(".cardmenu").css("opacity", 0);
+				$(".optionsmenu").css("opacity", 0);
+				$(".savemenu").css("opacity", 1);
+			});
+			// if($(".menu").css('opacity') === 0) {
+			// 	console.log("Menu is invisible");
+				$(".back").click(function() {
+					$(".menu").css('opacity', 1);
+					$(".pokemonmenu").css("opacity", 0);
+					$(".pokedexmenu").css("opacity", 0);
+					$(".bagmenu").css("opacity", 0);
+					$(".cardmenu").css("opacity", 0);
+					$(".optionsmenu").css("opacity", 0);
+					$(".savemenu").css("opacity", 0);
 				});
-				$("#pokedex").click(function() {
-
-				});
-				$("#bag").click(function() {
-
-				});
-				$("#card").click(function() {
-
-				});
-				$("#options").click(function() {
-
-				});
-				$("#save").click(function() {
-					
-				});
+			// }
 				status = "Open";
 			} else {
 				$(".menu").css('opacity', 0);
@@ -32,4 +88,5 @@ $(document).ready(function() {
 			}
 		}
 	});
+
 });
