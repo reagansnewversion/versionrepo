e21ed909b07420343f7874da6c07a551c3ad913b
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-12-22 16:56:43 -0600

    Moved the html for the menu to its own file to be included on pages where it can be utilized.

--- app/views/dexindex.blade.php
+++ app/views/dexindex.blade.php
@@ -15,52 +15,6 @@
 		</div>
 	</div>
 @endforeach
-<div class="menu">
-	<div class="menurow text-center col-lg-12 col-md-12">
-		<button type="submit" id="pokemon" class="menubutton pokemon col-lg-6 col-md-6">
-			Pokemon
-		</button>
-		<button id="pokedex" class="menubutton col-lg-6 col-md-6">
-			Pokedex
-		</button>
-	</div>
-	<div class="menurow text-center col-lg-12 col-md-12">
-		<button id="bag" class="menubutton col-lg-6 col-md-6">
-			Bag
-		</button>
-		<button id="card" class="menubutton col-lg-6 col-md-6">
-			Trainer Card
-		</button>
-	</div>
-	<div class="menurow text-center col-lg-12 col-md-12">
-		<button id="options" class="menubutton col-lg-6 col-md-6">
-			Options
-		</button>
-		<button id="save" class="menubutton col-lg-6 col-md-6">
-			Save
-		</button>
-	</div>
-</div>
-<div class="pokemonmenu">
-	Pokemon
-	<button class="back">Back</div>
-</div>
-<div class="pokedexmenu">
-	Pokedex
-	<button class="back">Back</button>
-</div>
-<div class="bagmenu">Bag
-	<button class="back">Back</button>
-</div>
-<div class="cardmenu">Trainer Card
-	<button class="back">Back</button>
-</div>
-<div class="optionsmenu">Options
-	<button class="back">Back</button>
-</div>
-<div class="savemenu">Save
-	<button class="back">Back</button>
-</div>
 @stop
 @section('bottom-script')
 <script type="text/javascript" src="/js/menu.js"></script>
