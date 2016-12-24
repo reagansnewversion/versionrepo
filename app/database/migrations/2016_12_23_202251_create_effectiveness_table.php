c262798e92f425c4a89e4e4a1a838d11f0eb4b55
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-12-23 20:02:14 -0600

    created a template that will make creating the effectiveness seeder easier

--- app/database/migrations/2016_12_23_202251_create_effectiveness_table.php
+++ app/database/migrations/2016_12_23_202251_create_effectiveness_table.php
@@ -16,10 +16,8 @@ class CreateEffectivenessTable extends Migration {
 			$table->increments('id');
 			$table->integer('move_type_id')->unsigned();
 			$table->foreign('move_type_id')->references('id')->on('types');
-			$table->integer('target_type1_id')->unsigned();
-			$table->foreign('target_type1_id')->references('id')->on('types');
-			$table->integer('target_type2_id')->unsigned()->nullable();
-			$table->foreign('target_type2_id')->references('id')->on('types');
+			$table->integer('target_type_id')->unsigned();
+			$table->foreign('target_type_id')->references('id')->on('types');
 			$table->string('no_effect')->nullable();
 			$table->string('not_very_effective')->nullable();
 			$table->string('effective')->nullable();
--- app/database/seeds/EffectivenessSeeder.php
+++ app/database/seeds/EffectivenessSeeder.php
@@ -11,21 +11,81 @@ class EffectivenessSeeder extends Seeder {
 		foreach($not as $n) {
 			$match = new Effectiveness();
 			$match->move_type_id = 1;
-			$match->target_type1_id = $n;
+			$match->target_type_id = $n;
 			$match->not_very_effective = 'true';
 			$match->save();
 		}
 		foreach($effective as $ef) {
 			$match = new Effectiveness();
 			$match->move_type_id = 1;
-			$match->target_type1_id = $ef;
+			$match->target_type_id = $ef;
 			$match->effective = 'true';
 			$match->save();
 		}
 		foreach($super as $s) {
 			$match = new Effectiveness();
 			$match->move_type_id = 1;
-			$match->target_type1_id = $n;
+			$match->target_type_id = $n;
+			$match->super_effective = 'true';
+			$match->save();
+		}
+
+		$not = [1, 5, 6, 17];
+		$effective = [2, 3, 4, 7, 8, 10, 11, 12, 13, 14, 16, 18];
+		$super = [9, 15];
+
+		foreach($not as $n) {
+			$match = new Effectiveness();
+			$match->move_type_id = 2;
+			$match->target_type_id = $n;
+			$match->not_very_effective = 'true';
+			$match->save();
+		}
+		foreach($effective as $ef) {
+			$match = new Effectiveness();
+			$match->move_type_id = 2;
+			$match->target_type_id = $ef;
+			$match->effective = 'true';
+			$match->save();
+		}
+		foreach($super as $s) {
+			$match = new Effectiveness();
+			$match->move_type_id = 2;
+			$match->target_type_id = $s;
+			$match->super_effective = 'true';
+			$match->save();
+		}
+
+		$noeffect = [5];
+		$not = [12];
+		$effective = [1, 2, 4, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18];
+		$super = [3];
+
+		foreach($noeffect as $ne) {
+			$match = new Effectiveness();
+			$match->move_type_id = 3;
+			$match->target_type_id = $ne;
+			$match->no_effect = 'true';
+			$match->save();
+		}
+		foreach($not as $n) {
+			$match = new Effectiveness();
+			$match->move_type_id = 3;
+			$match->target_type_id = $n;
+			$match->not_very_effective = 'true';
+			$match->save();
+		}
+		foreach($effective as $ef) {
+			$match = new Effectiveness();
+			$match->move_type_id = 3;
+			$match->target_type_id = $ef;
+			$match->effective = 'true';
+			$match->save();
+		} 
+		foreach($super as $s) {
+			$match = new Effectiveness();
+			$match->move_type_id = 3;
+			$match->target_type_id = $s;
 			$match->super_effective = 'true';
 			$match->save();
 		}
