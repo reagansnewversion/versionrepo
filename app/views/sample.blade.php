69c04587586788e1777d3b2a183915d025eb3708
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-12-31 14:02:29 -0600

    created a possible layout for a single row on the grid. If this works, it will be applied to the whole page

--- app/views/sample.blade.php
+++ app/views/sample.blade.php
@@ -1,8 +1,19 @@
 @extends('layouts.master')
 @section('content')
-<div class="row">
+<div class="row A">
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-		
+		<div class="A1 col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
+		<div class="A2 col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
+		<div class="A3 col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
+		<div class="A4 col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
+		<div class="A5 col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
+		<div class="A6 col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
+		<div class="A7 col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
+		<div class="A8 col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
+		<div class="A9 col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
+		<div class="A10 col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
+		<div class="A11 col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
+		<div class="A12 col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
 	</div>
 </div>
 @stop
