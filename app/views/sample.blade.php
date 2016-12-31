ee1ca6548a944ea8ef1c1529f244ea685f1358d2
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-12-31 14:21:51 -0600

    The styling works, so I am now layout out the full grid and seeing how many rows will fit in a single page.

--- app/views/sample.blade.php
+++ app/views/sample.blade.php
@@ -2,18 +2,34 @@
 @section('content')
 <div class="row A">
 	<div class="text-center col-lg-12 col-md-12 col-sm-12 col-xs-12">
-		<div id="A1" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">a</div>
-		<div id="A2" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">a</div>
-		<div id="A3" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">a</div>
-		<div id="A4" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">a</div>
-		<div id="A5" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">a</div>
-		<div id="A6" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">a</div>
-		<div id="A7" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">a</div>
-		<div id="A8" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">a</div>
-		<div id="A9" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">a</div>
-		<div id="A10" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">a</div>
-		<div id="A11" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">a</div>
-		<div id="A12" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">a</div>
+		<div id="A1" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">A1</div>
+		<div id="A2" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">A2</div>
+		<div id="A3" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">A3</div>
+		<div id="A4" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">A4</div>
+		<div id="A5" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">A5</div>
+		<div id="A6" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">A6</div>
+		<div id="A7" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">A7</div>
+		<div id="A8" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">A8</div>
+		<div id="A9" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">A9</div>
+		<div id="A10" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">A10</div>
+		<div id="A11" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">A11</div>
+		<div id="A12" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">A12</div>
+	</div>
+</div>
+<div class="row B">
+	<div class="text-center col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<div id="B1" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">B1</div>
+		<div id="B2" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">B2</div>
+		<div id="B3" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">B3</div>
+		<div id="B4" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">B4</div>
+		<div id="B5" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">B5</div>
+		<div id="B6" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">B6</div>
+		<div id="B7" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">B7</div>
+		<div id="B8" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">B8</div>
+		<div id="B9" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">B9</div>
+		<div id="B10" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">B10</div>
+		<div id="B11" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">B11</div>
+		<div id="B12" class="square col-lg-1 col-md-1 col-sm-1 col-xs-1">B12</div>
 	</div>
 </div>
 @stop
