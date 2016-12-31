d1a8f6bd3e0397f3239a42da12f89667e2e730d4
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-12-31 13:46:52 -0600

    Made stats nullable in the interest of being able to work on the game proper, since filling in the stats will take more time

--- app/database/migrations/2016_12_15_012838_create_pokemon_table.php
+++ app/database/migrations/2016_12_15_012838_create_pokemon_table.php
@@ -22,12 +22,12 @@ class CreatePokemonTable extends Migration {
 			$table->foreign('type2_id')->references('id')->on('types');
 			$table->integer('evolve_level')->unsigned()->nullable();
 			$table->string('evolve_method')->nullable();
-			$table->integer('base_hp')->unsigned();
-			$table->integer('base_attack')->unsigned();
-			$table->integer('base_sp_attack')->unsigned();
-			$table->integer('base_defense')->unsigned();
-			$table->integer('base_sp_defense')->unsigned();
-			$table->integer('base_speed')->unsigned();
+			$table->integer('base_hp')->unsigned()->nullable();
+			$table->integer('base_attack')->unsigned()->nullable();
+			$table->integer('base_sp_attack')->unsigned()->nullable();
+			$table->integer('base_defense')->unsigned()->nullable();
+			$table->integer('base_sp_defense')->unsigned()->nullable();
+			$table->integer('base_speed')->unsigned()->nullable();
 			$table->string('img_url')->nullable();
 			$table->string('entry', 1000000);
 			$table->timestamps();
