3e3d3327365fa5a2b1a08c61ca5ca2410696a247
Reagan Wilkins <reagan.wilkins@gmail.com>
2017-01-13 14:21:19 -0600

    created a template for the new seeder

--- app/database/seeds/LocationPokemonSquareSeeder.php
+++ app/database/seeds/LocationPokemonSquareSeeder.php
@@ -3,7 +3,10 @@ class LocationPokemonSquareSeeder extends Seeder
 {
 	public function run()
 	{
-		
+		$item = new LocationPokemonSquare();
+		$item->square_id = ;
+		$item->location_pokemon_id = ;
+		$item->save();
 	}
 }
 ?>
